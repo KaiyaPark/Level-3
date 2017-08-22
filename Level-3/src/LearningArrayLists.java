988b63e6a11742ab759754b7ca90068b72f69536
NickSanford4 <league@WTS22.attlocal.net>
2017-08-21 19:59:56 -0700

    ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Level-3/src/LearningArrayLists.java
+++ Level-3/src/LearningArrayLists.java
@@ -12,7 +12,7 @@ public class LearningArrayLists {
 	}
 
 	public static ArrayList getNumberOfItems(ArrayList testList) {
-		ArrayList<Integer> number = new ArrayList<Integer>(); 
+		ArrayList<Integer> number = new ArrayList<Integer>();
 		return number;
 	}
 
@@ -22,8 +22,31 @@ public class LearningArrayLists {
 	}
 
 	public static String iterateOver(ArrayList testList) {
-	
+		String t = "";
+		for (int i = 0; i < testList.size(); i++) {
+			String r = (String) testList.get(i);
+			t = t.concat(r);
+		}
+		return t;
+	}
+
+	public static Object findItemOnList(ArrayList testList, String string) {
+		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static ArrayList<String> createStringTypedArrayList() {
+		ArrayList<String> testList = new ArrayList<String>();
+		return testList;
+	}
+
+	public static String addAllString(ArrayList<String> testList) {
+		String w = "";
+		for (int i = 0; i < testList.size(); i++) {
+			String t = testList.get(i);
+			w = w.concat(t);
+		}
+		return w;
 	}
 
 }
